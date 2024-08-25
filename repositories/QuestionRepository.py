1535a8bbb6fc0f9eab8c5697dd5ceff4fe1ff4fc
XJIEI5 <egorchernov37@yandex.ru>
2024-08-25 22:46:17 +0300

    create question rep

---
+++ repositories/QuestionRepository.py
@@ -0,0 +1,18 @@
+from sqlalchemy import select
+
+from database.database import new_session
+from dtos.questions.question_create import CreateQuestion
+from models.QuestionModel import QuestionModel
+
+
+class QuestionRepository:
+    @classmethod
+    async def add_one(cls, question: CreateQuestion) -> int:
+        async with new_session() as session:
+            question_data = question.model_dump()
+
+            question = QuestionModel(**question_data)
+            session.add(question)
+            await session.flush()
+            await session.commit()
+            return question.ident
