935beed9428a8ecab319132472d96ad0664dd094
XJIEI5 <egorchernov37@yandex.ru>
2024-08-25 16:09:42 +0300

    simplyfied add_one

--- repositories/UserRepository.py
+++ repositories/UserRepository.py
@@ -1,17 +1,16 @@
-from dtos.users.user_regist import RegistUser
+from dtos.users.user_create import CreateUser
 from database.database import new_session
 from models.UserModel import UserModel
 
 class UserRepository:
     @classmethod
-    async def add_one(cls, data: RegistUser) -> int:
+    async def add_one(cls, data: CreateUser) -> int:
         async with new_session() as session:
             user_dict = data.model_dump()
 
             user = UserModel(**user_dict)
-            assert "password" in user_dict
-            user.set_hashed_password(user_dict["password"])
 
+            session.add(user)
             await session.flush()
             await session.commit()
-            return user.id
+            return user.UUID
