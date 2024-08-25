dd86fc68510c108f4fc756cb41c40f67207ec329
XJIEI5 <egorchernov37@yandex.ru>
2024-08-25 23:50:57 +0300

    added back_populates relation

--- models/QuestionModel.py
+++ models/QuestionModel.py
@@ -2,23 +2,26 @@ from typing import List, Optional
 from uuid import uuid4
 
 from models.BaseModel import BaseModel
+from models.TaskModel import TaskModel
 from models.AnswerModel import AnswerModel
 from sqlalchemy import ForeignKey
-from sqlalchemy import String, UUID, Integer
+from sqlalchemy import String, UUID, Integer, JSON
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.orm import relationship
-from helpers.enums.question_type_enum import QuestionType
-from helpers.enums.tablename_enum import TableName
+from helpers.enums.question_type_enum import QuestionTypeEnum
+from helpers.enums.tablename_enum import TableNameEnum
 
 
 class QuestionModel(BaseModel):
-    __tablename__ = TableName.QUESTIONS.value
+    __tablename__ = TableNameEnum.QUESTIONS.value
 
     UUID: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, unique=True, default=uuid4())
-    task_id: Mapped[UUID] = mapped_column(ForeignKey(f"{TableName.TASKS.value}.UUID"))
+    task_id: Mapped[UUID] = mapped_column(ForeignKey(f"{TableNameEnum.TASKS.value}.UUID"))
     question_body: Mapped[str] = mapped_column(String)
-    ident: Mapped[int] = mapped_column(Integer, unique=True, primary_key=True, autoincrement=True)
-    type: Mapped[QuestionType] = mapped_column(Integer)
-    content: Mapped[Optional[str]] = mapped_column(String)
+    identifier: Mapped[int] = mapped_column(Integer, unique=True, primary_key=True, autoincrement=True)
+    type: Mapped[QuestionTypeEnum] = mapped_column(QuestionTypeEnum)
+    content: Mapped[Optional[JSON]] = mapped_column(JSON)
     file_path: Mapped[Optional[str]] = mapped_column(String)
-    answers: Mapped[List[AnswerModel]] = relationship(cascade="all,delete")
+    
+    answers: Mapped[List[AnswerModel]] = relationship(cascade="all,delete", back_populates="question")
+    task: Mapped[TaskModel] = relationship(back_populates="questions")
