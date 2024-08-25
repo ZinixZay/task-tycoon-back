f88ec1b95579c55dff3c578220ca5cdd780e7607
XJIEI5 <egorchernov37@yandex.ru>
2024-08-25 22:44:12 +0300

    extended task model

--- models/TaskModel.py
+++ models/TaskModel.py
@@ -1,11 +1,21 @@
-from typing import Optional
+from typing import Optional, List
+from uuid import uuid4
+
+from sqlalchemy.orm import relationship
+from sqlalchemy import String, UUID, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 from models.BaseModel import BaseModel
+from models.QuestionModel import QuestionModel
+from helpers.enums.tablename_enum import TableName
 
 
 class TaskModel(BaseModel):
-    __tablename__ = "tasks"
+    __tablename__ = TableName.TASKS.value
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str]
-    description: Mapped[Optional[str]]
+    UUID: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, unique=True, default=uuid4())
+    title: Mapped[str] = mapped_column(String)
+    ident: Mapped[int] = mapped_column(Integer, unique=True, primary_key=True, autoincrement=True)
+    description_full: Mapped[str] = mapped_column(String)
+    description_short: Mapped[str] = mapped_column(String)
+    file_path: Mapped[Optional[str]] = mapped_column(String)
+    questions: Mapped[List[QuestionModel]] = relationship(cascade="all,delete")
