34902d304174972972c2b4d7402631c9c150e89a
XJIEI5 <egorchernov37@yandex.ru>
2024-08-25 23:50:53 +0300

    added back_populates relation

--- models/AnswerModel.py
+++ models/AnswerModel.py
@@ -2,18 +2,20 @@ from typing import Any, Optional
 from uuid import uuid4
 
 from models.BaseModel import BaseModel
-from sqlalchemy import ForeignKey
-from sqlalchemy import String, UUID, Integer
-from sqlalchemy.orm import Mapped, mapped_column
-from helpers.enums.question_type_enum import QuestionType
-from helpers.enums.tablename_enum import TableName
+from models.AnswerModel import AnswerModel
+from models.UserModel import UserModel
+from sqlalchemy import String, UUID, ForeignKey, JSON
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from helpers.enums.tablename_enum import TableNameEnum
 
 
 class AnswerModel(BaseModel):
-    __tablename__ = TableName.ANSWERS.value
+    __tablename__ = TableNameEnum.ANSWERS.value
 
     UUID: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, unique=True, default=uuid4())
-    question_id: Mapped[UUID] = mapped_column(ForeignKey(f"{TableName.QUESTIONS.value}.UUID"))
-    user_id: Mapped[UUID] = mapped_column(ForeignKey(f"{TableName.USERS.value}.UUID"))
-    content: Mapped[Optional[str]] = mapped_column(String)
-    answer_text: Mapped[Optional[str]] = mapped_column(String)
+    question_id: Mapped[UUID] = mapped_column(ForeignKey(f"{TableNameEnum.QUESTIONS.value}.UUID"))
+    user_id: Mapped[UUID] = mapped_column(ForeignKey(f"{TableNameEnum.USERS.value}.UUID"))
+    content: Mapped[JSON] = mapped_column(JSON)
+
+    question: Mapped[AnswerModel] = relationship(back_populates="answers")
+    user: Mapped[UserModel] = relationship(back_populates="answers")
