075556704eb9bddd51e8f26afedea3a3fbf6c8a1
XJIEI5 <egorchernov37@yandex.ru>
2024-08-25 23:50:59 +0300

    added back_populates relation

--- models/TaskModel.py
+++ models/TaskModel.py
@@ -1,9 +1,8 @@
 from typing import Optional, List
 from uuid import uuid4
 
-from sqlalchemy.orm import relationship
 from sqlalchemy import String, UUID, Integer
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from models.BaseModel import BaseModel
 from models.QuestionModel import QuestionModel
 from helpers.enums.tablename_enum import TableName
@@ -14,8 +13,9 @@ class TaskModel(BaseModel):
 
     UUID: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, unique=True, default=uuid4())
     title: Mapped[str] = mapped_column(String)
-    ident: Mapped[int] = mapped_column(Integer, unique=True, primary_key=True, autoincrement=True)
-    description_full: Mapped[str] = mapped_column(String)
-    description_short: Mapped[str] = mapped_column(String)
+    identifier: Mapped[int] = mapped_column(Integer(6), unique=True, primary_key=True, autoincrement=True)
+    description_full: Mapped[Optional[str]] = mapped_column(String)
+    description_short: Mapped[Optional[str]] = mapped_column(String)
     file_path: Mapped[Optional[str]] = mapped_column(String)
-    questions: Mapped[List[QuestionModel]] = relationship(cascade="all,delete")
+    
+    questions: Mapped[List[QuestionModel]] = relationship(cascade="all,delete", back_populates="task")
