64b7e3d0d12060fccf676fcd203f9ef0d27fd580
XJIEI5 <egorchernov37@yandex.ru>
2024-08-31 21:13:41 +0300

    make return entity instead of UUID

--- repositories/UserRepository.py
+++ repositories/UserRepository.py
@@ -1,11 +1,10 @@
 from dtos.users.user_create import CreateUser
 from database.database import get_async_session
 from models.UserModel import UserModel
-from uuid import UUID
 
 class UserRepository:
     @classmethod
-    async def add_one(cls, data: CreateUser) -> UUID:
+    async def add_one(cls, data: CreateUser) -> UserModel:
         async for session in get_async_session():
             user_dict = data.model_dump()
 
@@ -14,4 +13,4 @@ class UserRepository:
             session.add(user)
             await session.flush()
             await session.commit()
-            return user.id
+            return user
