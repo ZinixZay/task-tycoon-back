f536983faf2aabcfe1752754e22a5dab6d3b3589
XJIEI5 <egorchernov37@yandex.ru>
2024-08-31 16:23:38 +0300

    fix add_one

--- repositories/QuestionRepository.py
+++ repositories/QuestionRepository.py
@@ -1,12 +1,13 @@
 from database.database import get_async_session
 from dtos.questions.question_create import CreateQuestion
 from models.QuestionModel import QuestionModel
+from uuid import UUID
 
 
 class QuestionRepository:
     @classmethod
-    async def add_one(cls, question: CreateQuestion) -> int:
-        async with get_async_session() as session:
+    async def add_one(cls, question: CreateQuestion) -> UUID:
+        async for session in get_async_session():
             question_data = question.model_dump()
 
             question = QuestionModel(**question_data)
