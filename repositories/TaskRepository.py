d3b1eced23457196ad61ed7447eedd3b28523995
XJIEI5 <egorchernov37@yandex.ru>
2024-08-31 16:23:46 +0300

    fix add_one

--- repositories/TaskRepository.py
+++ repositories/TaskRepository.py
@@ -4,12 +4,13 @@ from database.database import get_async_session
 from dtos.tasks.task_create import CreateTask
 from dtos.tasks.task_get import GetTask
 from models.TaskModel import TaskModel
+from uuid import UUID
 
 
 class TaskRepository:
     @classmethod
-    async def add_one(cls, data: CreateTask) -> int:
-        async with get_async_session() as session:
+    async def add_one(cls, data: CreateTask) -> UUID:
+        async for session in get_async_session():
             task_dict = data.model_dump()
 
             task = TaskModel(**task_dict)
@@ -20,7 +21,7 @@ class TaskRepository:
 
     @classmethod
     async def find_all(cls) -> list[GetTask]:
-        async with get_async_session() as session:
+        async for session in get_async_session():
             query = select(TaskModel)
             result = await session.execute(query)
             task_models = result.scalars().all()
