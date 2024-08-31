6ef015a01b7a2b3e5dd9ed9204dfb4a914c3a847
XJIEI5 <egorchernov37@yandex.ru>
2024-08-31 21:43:59 +0300

    add content to CreateQuestion

--- dtos/questions/question_create.py
+++ dtos/questions/question_create.py
@@ -1,11 +1,18 @@
+from typing import Optional
 from pydantic import BaseModel
 from uuid import UUID
+
+
+class ContentField(BaseModel):
+    title: str
+    is_correct: bool
 
 
 class CreateQuestion(BaseModel):
     question_body: str
     type: int
     task_id: UUID
+    content: Optional[list[ContentField]]
 
 
 class CreateQuestionResponse(BaseModel):
