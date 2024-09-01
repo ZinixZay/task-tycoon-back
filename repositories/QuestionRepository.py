fe320b6a2b244224aff89b777673c217bfe99867
XJIEI5 <egorchernov37@yandex.ru>
2024-09-01 11:49:47 +0300

    add question get route

--- repositories/QuestionRepository.py
+++ repositories/QuestionRepository.py
@@ -1,7 +1,9 @@
+from sqlalchemy import select, exc
+from typing import List
 from database.database import get_async_session
-from dtos import CreateQuestion
 from models.QuestionModel import QuestionModel
 from uuid import UUID
+from utils.custom_errors import NotFoundException
 
 
 class QuestionRepository:
@@ -12,3 +14,23 @@ class QuestionRepository:
             await session.flush()
             await session.commit()
             return question
+    
+    @classmethod
+    async def find_by_task(cls, task_id: UUID) -> List[QuestionModel]:
+        async for session in get_async_session():
+            query = select(QuestionModel).where(QuestionModel.task_id == task_id)
+            result = await session.execute(query)
+            question_entity = result.scalars().all()
+            # NOTE: no check for emptyness is intended
+            return question_entity
+        
+    @classmethod
+    async def find(cls, id: UUID) -> QuestionModel:
+        async for session in get_async_session():
+            query = select(QuestionModel).where(QuestionModel.id == id)
+            result = await session.execute(query)
+            try:
+                question_entity = result.scalars().one()
+            except exc.NoResultFound:
+                raise NotFoundException({"question_id": str(id)})
+            return question_entity
--- routers/question_router.py
+++ routers/question_router.py
@@ -1,7 +1,29 @@
+from typing import List
 from fastapi import APIRouter
+from dtos import GetQuestionResponse
+from models import QuestionModel
+from repositories import QuestionRepository
+from uuid import UUID
 
 
 questions_router: APIRouter = APIRouter(
     prefix="/questions",
     tags=["Вопросы"],
 )
+
+@questions_router.get("/by_task/{task_id}")
+async def get_questions_by_task(
+    task_id: UUID
+) -> List[GetQuestionResponse]:
+    question_entities: List[QuestionModel] = await QuestionRepository.find_by_task(task_id)
+    question_schemas = [GetQuestionResponse.model_validate(i) for i in question_entities]
+    return question_schemas
+
+
+@questions_router.get("/by_id/{question_id}")
+async def get_question_by_id(
+    question_id: UUID
+) -> GetQuestionResponse:
+    question_entity: QuestionModel = await QuestionRepository.find(question_id)
+    question_shema = GetQuestionResponse.model_validate(question_entity)
+    return question_shema
