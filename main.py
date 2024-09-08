d0223a7c9924714c10c608b8950f5e0d2da56fcb
ZinixZay <romadesl21@gmail.com>
2024-09-08 15:38:53 +0300

    added path

--- main.py
+++ main.py
@@ -25,4 +25,4 @@ app.include_router(fastapi_users.get_register_router(GetUser, CreateUser), prefi
 app.include_router(fastapi_users.get_users_router(GetUser, UpdateUser), prefix='/auth/jwt', tags=['users'])
 app.include_router(tasks_router)
 app.include_router(questions_router)
-app.include_router(permission_router)
+app.include_router(permission_router, prefix="/api/v1")
