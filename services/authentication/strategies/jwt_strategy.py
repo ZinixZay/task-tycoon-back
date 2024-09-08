f6d4edcb36c7414cf1743bf8b5ae7c3696a714b1
ZinixZay <romadesl21@gmail.com>
2024-09-08 20:38:02 +0300

    changed lifetime token

--- services/authentication/strategies/jwt_strategy.py
+++ services/authentication/strategies/jwt_strategy.py
@@ -4,4 +4,4 @@ from utils.env.get_env_variables import EnvironmentVariables
 SECRET = EnvironmentVariables.JWT_SECRET.value
 
 def get_jwt_strategy() -> JWTStrategy:
-    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+    return JWTStrategy(secret=SECRET, lifetime_seconds=604800)
