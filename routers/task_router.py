0b47b4da793083b9e4ed094cfccc5b303b3015f4
ZinixZay <romadesl21@gmail.com>
2024-09-14 00:02:25 +0300

    fix

--- routers/task_router.py
+++ routers/task_router.py
@@ -2,7 +2,7 @@ from typing import List
 from fastapi import APIRouter, Depends
 from dtos.questions import Question
 from dtos.tasks import GetTasksResponse, IsolatedTask, GetTasksByUserDto, GetTasksByTitleDto, \
-    GetTaskByIdentifierDto, FullTaskResponse, DeleteTaskByIdDto, CreateTaskResponse, CreateTaskDto
+    GetTaskByIdentifierDto, FullTaskResponse, GetTaskByIdDto, DeleteTaskByIdDto, CreateTaskResponse, CreateTaskDto
 from dtos.transactions.transaction import TransactionPayload
 from repositories import TaskRepository
 from services.authentication import fastapi_users
@@ -108,21 +108,21 @@ async def get_task_by_id(
 
 @tasks_router.delete("/")
 async def delete_task_by_id(
-        task_id: UUID,
+        query_params: DeleteTaskByIdDto = Depends(),
         user_entity: UserModel = Depends(fastapi_users.current_user())
 ) -> UUID:
     if user_entity.is_superuser:
-        await TaskRepository.delete_by_id(task_id)
-        return task_id
+        await TaskRepository.delete_by_id(query_params.task_id)
+        return query_params.task_id
     
-    task_entity: TaskModel = await TaskRepository.find_by_id(task_id)
+    task_entity: TaskModel = await TaskRepository.find_by_id(query_params.task_id)
     if task_entity is None:
-        raise NotFoundException({"not found": task_id})
+        raise NotFoundException({"not found": query_params.task_id})
     
     task_was_added_by_another_user = task_entity.user_id != user_entity.id
     user_has_permission = Permissions.from_number(user_entity.permissions).has(PermissionsEnum.DeleteOthersTasks)
     if not user_has_permission and task_was_added_by_another_user:
         raise NoPermissionException(PermissionsEnum.DeleteOthersTasks)
-    await TaskRepository.delete_by_id(task_id)
+    await TaskRepository.delete_by_id(query_params.task_id)
             
-    return task_id
+    return query_params.task_id
