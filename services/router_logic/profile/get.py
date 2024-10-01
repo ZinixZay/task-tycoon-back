efdf9608bb27c91afd151c88e42b640cc444bee0
XJIEI5 <egorchernov37@yandex.ru>
2024-10-01 17:05:21 +0300

    add email

--- services/router_logic/profile/get.py
+++ services/router_logic/profile/get.py
@@ -8,6 +8,7 @@ async def profile_get(
     user_entity: UserModel = Depends(fastapi_users.current_user())
 ) -> GetProfileResponse:
     return GetProfileResponse(
+        email=user_entity.email,
         nickname=user_entity.nickname,
         name=user_entity.name,
         surname=user_entity.surname
