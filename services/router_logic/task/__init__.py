d704431f4d468c87510690106c137e7a1e6dbb05
XJIEI5 <egorchernov37@yandex.ru>
2024-10-04 23:45:58 +0300

    hotfix

--- services/router_logic/task/__init__.py
+++ services/router_logic/task/__init__.py
@@ -2,7 +2,7 @@ from services.router_logic.task.add import *
 from services.router_logic.task.delete_by_id import *
 from services.router_logic.task.get_by_id_to_observe import *
 from services.router_logic.task.get_by_id_to_solve import *
-from services.router_logic.task.get_by_identifier import *
+from services.router_logic.task.task_get_by_id_without_questions import *
 from services.router_logic.task.get_by_title import *
 from services.router_logic.task.get_by_user import *
 from services.router_logic.task.get_many import *
