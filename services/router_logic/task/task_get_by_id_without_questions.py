d153f3d2088e928586642c46ca8a8efbac65f6d9
ZinixZay <romadesl21@gmail.com>
2024-10-05 17:53:11 +0300

    w

--- services/router_logic/task/task_get_by_id_without_questions.py
+++ services/router_logic/task/task_get_by_id_without_questions.py
@@ -1,12 +1,22 @@
 from fastapi import Depends
 from dtos.tasks import IsolatedTask, GetTaskByIdWithoutQuestions
+from dtos.tasks.task import GetWithoutQuestions
+from models import UserModel
 from repositories import TaskRepository
+from services.authentication import fastapi_users
 
 
 async def task_get_by_id_without_questions(
-    query_params: GetTaskByIdWithoutQuestions = Depends()
-) -> IsolatedTask:
-    identifier = query_params.id
-    task_entity = await TaskRepository.find_by_id(identifier)
+    query_params: GetTaskByIdWithoutQuestions = Depends(),
+    user_entity: UserModel = Depends(fastapi_users.current_user())
+) -> GetWithoutQuestions:
+    id = query_params.id
+    task_entity = await TaskRepository.find_by_id(id)
+    response: GetWithoutQuestions
     result: IsolatedTask = IsolatedTask.model_validate(task_entity.__dict__)
-    return result
+    if task_entity.user_id == user_entity.id:
+        response.mode = 'full'
+    else:
+        response.mode = 'general'
+    response.task = task_entity
+    return response
