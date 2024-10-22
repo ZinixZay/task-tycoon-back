9c6265ba55e6318b033b22d93610d37073194173
ZinixZay <romadesl21@gmail.com>
2024-10-23 00:28:58 +0300

    w

--- services/router_logic/stats/get_task_stats_resulting.py
+++ services/router_logic/stats/get_task_stats_resulting.py
@@ -15,10 +15,14 @@ async def stats_get_resulting_attempt(
         raise ForbiddenException('Недостаточно прав')
     resulting_attempts = await SummaryStatsRepository.get_by_task(task_id)
     result: List[TaskStatsResultingResponse] = []
+    if user.name or user.surname:
+        user_initials = ' '.join([user.name if user.name else '', user.surname if user.surname else ''])
+    else:
+        user_initials = user.nickname if user.nickname else user.email
     for stats in resulting_attempts:
         result.append(TaskStatsResultingResponse(
             user_id=user.id,
-            user_initials=' '.join([user.name, user.surname]),
+            user_initials=user_initials,
             best_result=stats.best_result,
             avg_result=stats.avg_result,
             attempt_amount=stats.attempt_amount
