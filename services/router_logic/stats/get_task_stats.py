8e3b379f99e4d093de77150d4916a17c5c242e91
ZinixZay <romadesl21@gmail.com>
2024-10-24 21:54:27 +0300

    w

--- services/router_logic/stats/get_task_stats.py
+++ services/router_logic/stats/get_task_stats.py
@@ -23,7 +23,7 @@ async def stats_get_task(
         cached_stats = await Cache.get(f"stats_task_{task.id}")
         if (not cached_stats):
             cached_stats = await TaskStatsCalculate.calculate_task_stats(task.id)
-            cached_stats = json.loads(json.loads(cached_stats))
+            cached_stats = json.loads(cached_stats)
         else:
             cached_stats: dict = json.loads(json.loads(cached_stats))
         cached_stats['task_id'] = task.id
