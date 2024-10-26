d273a68698ed8895c2e8fcd8ad2841aeda1ef612
XJIEI5 <egorchernov37@yandex.ru>
2024-10-26 13:45:11 +0300

    в родную гавань

--- routers/export_router.py
+++ routers/export_router.py
@@ -15,6 +15,6 @@ export_router: APIRouter = APIRouter(
 @export_router.get("/stats/excel")
 async def export_stats_excel(
     task_id: UUID,
-    # user: UserModel = Depends(fastapi_users.current_user())
+    user: UserModel = Depends(fastapi_users.current_user())
 ) -> FileResponse:
-    return await export_excel(task_id) # , user)
+    return await export_excel(task_id, user)
