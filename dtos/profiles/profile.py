504bc25ec5b86fdf7f75c6d533a767fc37e82845
ZinixZay <romadesl21@gmail.com>
2024-10-30 10:37:25 +0300

    w

--- dtos/profiles/profile.py
+++ dtos/profiles/profile.py
@@ -1,6 +1,7 @@
+import re
 from typing import Optional
 from uuid import UUID
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, field_validator
 
 
 class GetProfileResponse(BaseModel):
@@ -11,7 +12,23 @@ class GetProfileResponse(BaseModel):
 
 
 class UpdateProfileDto(BaseModel):
-    email: str
-    nickname: str
-    name: str
-    surname: str
+    email: Optional[EmailStr] = None
+    nickname: Optional[str] = None
+    name: Optional[str] = None
+    surname: Optional[str] = None
+    
+    @field_validator('nickname')
+    def validate_nickname(cls, val: str) -> str:
+        if not (3 <= len(val) <= 20):
+            raise ValueError('Длина никнейма должна быть от 3 до 20')
+        if not (re.match(r'^[a-zA-Z0-9_]+$', val)):
+            raise ValueError('Некорректный никнейм')
+        return val
+
+    # @field_validator('name')
+    # def validate_name(cls, val: str) -> str:
+    #     if not (3 <= len(val) <= 20):
+    #         raise ValueError('Длина никнейма должна быть от 3 до 20')
+    #     if not (re.match(r'^[a-zA-Z0-9_]+$', val)):
+    #         raise ValueError('Некорректный никнейм')
+    #     return val
