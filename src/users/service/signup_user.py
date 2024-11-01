1b7b195a18e345f7b779d637bb21b7fea77f10f9
ZinixZay <romadesl21@gmail.com>
2024-11-01 21:38:20 +0300

    w

--- src/users/service/signup_user.py
+++ src/users/service/signup_user.py
@@ -1,12 +1,12 @@
 from pydantic import EmailStr
-from src.entity.UserEntity import UserEntity
+from src.entity.UserEntity import UserEntity as User
 from src.users.dto import RegisterUserDto
 from src.helpers.errors import BadRequestException
 
 
 async def signup_user(user_register_dto: RegisterUserDto) -> EmailStr:
     try:
-        user: UserEntity = UserEntity.create(email=user_register_dto.email, hashed_password=User.hash_password(user_register_dto.password))
+        user: User = User.create(email=user_register_dto.email, hashed_password=User.hash_password(user_register_dto.password))
     except Exception as e:
         print(e)
         raise BadRequestException(f'Не удалось создать пользователя. (Возможно пользователь {user_register_dto.email} уже существует)')
