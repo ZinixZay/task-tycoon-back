b825f72cb1c77c412a743c2aaf854ea42fba98c7
ZinixZay <romadesl21@gmail.com>
2024-11-17 18:52:36 +0300

    w

--- src/questions/service/delete_questions.py
+++ src/questions/service/delete_questions.py
@@ -2,16 +2,14 @@ from typing import List
 from uuid import UUID
 from src.jwt_strategy.dto import TokenDto
 from src.entity.UserEntity import UserEntity as User
-from src.helpers.errors import NotFoundException, ForbiddenException
-from src.users.dto.enums import UserRolesEnum
+from src.helpers.errors import NotFoundException
+from src.entity.QuestionEntity import QuestionEntity as Question
 
 
 def delete_questions(user: TokenDto, question_ids: List[UUID]):
     user_entity: User | None = User.get_or_none(User.id == user.user_id)
     if not user_entity:
         raise NotFoundException("Пользователь не найден")
-    if user_entity.is_superuser:
-        pass
 
-    прив
+    question_entities: List[Question] = list(Question.select().where(Question.id.in_(question_ids)).execute())
     
